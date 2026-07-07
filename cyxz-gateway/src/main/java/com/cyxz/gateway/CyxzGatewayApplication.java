078abc10bd02c1651f6d2ad6614f17eebeafc695
touches <16770392+touches@user.noreply.gitee.com>
2026-07-07 22:41:44 +0800

    style: 统一移除 @EnableDiscoveryClient
    - Spring Cloud 2022+ 中 Nacos starter 自动完成服务注册
    - 统一所有启动类风格，不再显式声明该注解

--- cyxz-gateway/src/main/java/com/cyxz/gateway/CyxzGatewayApplication.java
+++ cyxz-gateway/src/main/java/com/cyxz/gateway/CyxzGatewayApplication.java
@@ -2,10 +2,8 @@ package com.cyxz.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CyxzGatewayApplication {
 
     public static void main(String[] args) {
