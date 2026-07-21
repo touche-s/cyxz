1ab2cccd1d4bf6c153fdec4e317fb0d2093bc92d
touches <16770392+touches@user.noreply.gitee.com>
2026-07-22 02:26:50 +0800

    chore(frontend): 更新工程配置与全局样式，集成图标素材库

--- cyxz-frontend/vite.config.ts
+++ cyxz-frontend/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
