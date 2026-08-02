b2e581a80b4bf359b8ea56782b27823f734a0b1e
touches <16770392+touches@user.noreply.gitee.com>
2026-08-02 21:29:00 +0800

    refactor(comment): 适配 message-api 包路径扁平化

--- cyxz-comment/src/main/java/com/cyxz/comment/CyxzCommentApplication.java
+++ cyxz-comment/src/main/java/com/cyxz/comment/CyxzCommentApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.cyxz.comment.mapper")
-@SpringBootApplication(scanBasePackages = {"com.cyxz.comment", "com.cyxz.common", "com.cyxz.user.feign", "com.cyxz.post.feign", "com.cyxz.message.api.feign"})
-@EnableFeignClients(basePackages = {"com.cyxz.user.feign", "com.cyxz.post.feign", "com.cyxz.message.api.feign"})
+@SpringBootApplication(scanBasePackages = {"com.cyxz.comment", "com.cyxz.common", "com.cyxz.user.feign", "com.cyxz.post.feign", "com.cyxz.message.feign"})
+@EnableFeignClients(basePackages = {"com.cyxz.user.feign", "com.cyxz.post.feign", "com.cyxz.message.feign"})
 @EnableScheduling
 public class CyxzCommentApplication {
 
